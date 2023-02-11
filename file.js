9a858e49a090cc477326cef58d7c1200f44f6141
Anshita <Anshita@email.com>
2023-02-11 19:52:21 +0530

    Inti

---
+++ file.js
@@ -0,0 +1,17 @@
+//console.log("anshita");
+/*
+let newString:string="anshita yadav";
+
+console.log(newString);
+
+let fun:Array<string>=["vaishu","ayu","ankita"];
+
+console.log(fun);
+
+let Person:Array<string|number>=["anshita","20","mp"];
+
+console.log(Person);
+
+*/
+var num = " anshita tilak";
+console.log(num);
---
+++ file.ts
@@ -0,0 +1,21 @@
+
+//console.log("anshita");
+
+/*
+let newString:string="anshita yadav";
+
+console.log(newString);
+
+let fun:Array<string>=["vaishu","ayu","ankita"];
+
+console.log(fun);
+
+let Person:Array<string|number>=["anshita","20","mp"];
+
+console.log(Person);
+
+*/
+
+let num:string=" anshita tilak";
+
+console.log(num);
